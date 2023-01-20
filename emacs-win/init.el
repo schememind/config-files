4316974b1b5d3afc68b380fd64b8073adc6071f1
Žans Kļimovičs <4805170+zkrolllock@users.noreply.github.com>
2023-01-20 10:06:50 +0200

    Changed projectile key binding to C-c p

--- emacs-win/init.el
+++ emacs-win/init.el
@@ -99,7 +99,7 @@
 (use-package projectile
   :ensure t
   :config
-  (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (projectile-mode +1))
 (use-package helm-projectile
   :ensure t
